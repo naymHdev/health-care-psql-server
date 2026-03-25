1b86a36e963b513c3c98fb62f42189aaf7accc12
naymHdev <naym100m@gmail.com>
2026-03-25 19:07:21 +0600

    created shorting helper file

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Application, Request, Response, urlencoded } from "express";
+import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import router from "./app/routes";
 
@@ -9,7 +9,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/v1", router);
-
 app.get("/", (req: Request, res: Response) => {
   res.send("Health Care API is running on port http://localhost:8000");
 });
